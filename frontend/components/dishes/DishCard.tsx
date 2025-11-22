8d39639cce89e9e2c3d86467efe6684c24e85b3e
Vlad Korzun <vlad.korzun.minsk@gmail.com>
2025-11-22 21:38:11 +0300

    update DishCard.tsx with new database Api

--- frontend/components/dishes/DishCard.tsx
+++ frontend/components/dishes/DishCard.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { useState, useEffect } from "react";
+import Api from "@/app/api/db_api";
 
 interface Ingredient {
   name: string;
@@ -20,48 +21,38 @@ interface RecipeDetailsProps {
 }
 
 export default function DishCard({ id }: RecipeDetailsProps) {
-  const [recipe, setRecipe] = useState<RecipeDetails | null>(null);
+  const [recipe, setRecipe] = useState<any>(null);
   useEffect(() => {
     const fetchRecipe = async () => {
-      const response = await fetch(`/api/recipes/${id}`);
-      const data = await response.json();
-      setRecipe(data);
+      const response = await Api.getRecipe(id);
+      const response2 = await Api.getIngredients(id);
+      setRecipe({ ...response, ingredients: response2 });
     };
     fetchRecipe();
   }, [id]);
   if (!recipe) return <div>Loading...</div>;
+
   return (
     <div className="fixed right-0 top-16 h-[calc(100vh-4rem)] w-80 bg-white shadow-lg border-l transform transition-transform duration-300">
       <div className="p-6 h-full overflow-y-auto">
-        {/* Кнопка закрытия */}
-        {/* <button
-          onClick={onClose}
-          className="mb-6 text-gray-500 hover:text-gray-700 text-lg">
-          ✕ Закрыть
-        </button> */}
-
-        {/* Заголовок и описание */}
         <h2 className="text-2xl font-bold mb-4">{recipe.title}</h2>
         <p className="text-gray-700 mb-6">{recipe.description}</p>
-
-        {/* Ингредиенты */}
         <div className="mb-6">
           <h3 className="text-lg font-semibold mb-3">Ингредиенты:</h3>
           <div className="space-y-2">
-            {recipe.ingredients.map((ingredient, index) => (
+            {recipe.ingredients.map((ingredient: any, index: number) => (
               <div
                 key={index}
                 className="flex justify-between items-center py-1 border-b border-gray-100">
-                <span className="text-gray-700">{ingredient.name}</span>
+                <span className="text-gray-700">{ingredient.product_name}</span>
                 <span className="text-gray-500 text-sm">
-                  {ingredient.quantity} {ingredient.unit}
+                  {ingredient.amount} {ingredient.product_unit}
                 </span>
               </div>
             ))}
           </div>
         </div>
 
-        {/* Инструкции */}
         <div className="mb-6">
           <h3 className="text-lg font-semibold mb-3">Способ приготовления:</h3>
           <p className="text-gray-700 whitespace-pre-line">
