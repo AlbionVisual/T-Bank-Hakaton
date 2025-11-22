03d727089e1d4d237c56dc0848e693102db2bdee
Anton <antongl1608@gmail.com>
2025-11-22 14:53:18 +0300

    start flask

--- Back-end/app.py
+++ Back-end/app.py
@@ -1,10 +1,15 @@
-import flask
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+from database import *
+from query_openrouter import query_openrouter
 
-app = flask.Flask(__name__)
+app = Flask(__name__)
+CORS(app)
 
-@app.route('/')
-def index():
-    return "Hello, World!"
+@app.route('/api/chats', methods=['GET'])
+def get_dichae():
+    return jsonify(dialog_list_request(dict_return=True))
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=5000)
