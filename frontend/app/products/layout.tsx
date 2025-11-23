44c764b9ac5a3e34834b93243b71d5bbe00af607
Vlad Korzun <vlad.korzun.minsk@gmail.com>
2025-11-23 05:46:26 +0300

    add add icon on almost every page

--- frontend/app/products/layout.tsx
+++ frontend/app/products/layout.tsx
@@ -1,17 +1,94 @@
 "use client";
+import { Dialog } from "@/components/ui/Dialog";
 import { ListItems } from "@/components/ui/List";
+import { PlusButton } from "@/components/ui/PlusButton";
+import { useEffect, useState } from "react";
+import Api from "../api/db_api";
+
+interface Product {
+  id: number;
+  name: string;
+  unit: string;
+}
 
 export default function ProductsLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const [showDialog, setShowDialog] = useState(false);
+  const [productName, setProductName] = useState("");
+  const [productUnit, setProductUnit] = useState("");
+  const [refreshKey, setRefreshKey] = useState(0);
+  const [products, setProducts] = useState<Product[]>([]);
+  const [acceptationDisabled, setAcceptationDisabled] = useState(false);
+  useEffect(() => {
+    Api.getProducts().then((products) => {
+      setProducts(products as Product[]);
+    });
+  }, []);
+
+  const handleAdd = async () => {
+    await Api.createProduct({ name: productName, unit: productUnit });
+    setShowDialog(false);
+    setProductName("");
+    setProductUnit("");
+    setRefreshKey((prev) => prev + 1);
+  };
+
+  const handleCancel = () => {
+    setShowDialog(false);
+    setProductName("");
+    setProductUnit("");
+  };
+
+  const handleNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setProductName(e.target.value);
+    if (products.find((product) => product.name === e.target.value))
+      setAcceptationDisabled(true);
+    else setAcceptationDisabled(false);
+  };
+
   return (
-    <ListItems
-      url="products"
-      renderItem={(item, index) => children}
-      keyExtractor={(item: any) => item.id.toString()}
-      headerText="Продукты"
-    />
+    <>
+      <ListItems
+        url="products"
+        renderItem={(item, index) => children}
+        keyExtractor={(item: any) => item.id.toString()}
+        headerText="Продукты"
+        refreshKey={refreshKey}
+      />
+      <PlusButton
+        onClick={() => {
+          setShowDialog(true);
+        }}
+      />
+      {showDialog && (
+        <Dialog
+          on_add={handleAdd}
+          on_cancel={handleCancel}
+          disabled={acceptationDisabled}>
+          <div className="h-full w-full flex flex-col items-center justify-center">
+            <h1 className="text-2xl font-bold text-white border-b-2 border-yellow-300 mb-8">
+              Добавить продукт
+            </h1>
+            <input
+              type="text"
+              className="w-7/8 p-2 rounded-md bg-white"
+              placeholder="Название продукта"
+              value={productName}
+              onChange={handleNameChange}
+            />
+            <input
+              type="text"
+              className="w-7/8 p-2 rounded-md bg-white"
+              placeholder="Единица измерения"
+              value={productUnit}
+              onChange={(e) => setProductUnit(e.target.value)}
+            />
+          </div>
+        </Dialog>
+      )}
+    </>
   );
 }
--- frontend/app/recipes/layout.tsx
+++ frontend/app/recipes/layout.tsx
@@ -1,23 +1,84 @@
 "use client";
+import { Dialog } from "@/components/ui/Dialog";
 import { ListItems } from "@/components/ui/List";
+import { PlusButton } from "@/components/ui/PlusButton";
+import { useEffect, useState } from "react";
+import Api from "../api/db_api";
+
+interface Recipe {
+  name: string;
+  description: string | undefined;
+  instructions: string | undefined;
+  id: number;
+}
+
 export default function RecepiesLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const [showDialog, setShowDialog] = useState(false);
+  const [recipeName, setRecipeName] = useState("");
+  const [refreshKey, setRefreshKey] = useState(0);
+  const [recipes, setRecipes] = useState<Recipe[]>([]);
+  const [acceptationDisabled, setAcceptationDisabled] = useState(false);
+  useEffect(() => {
+    Api.getRecipes().then((recipes) => setRecipes(recipes as Recipe[]));
+  }, []);
+
+  const handleAdd = async () => {
+    await Api.createRecipe({ name: recipeName });
+    setShowDialog(false);
+    setRecipeName("");
+    setRefreshKey((prev) => prev + 1);
+  };
+
+  const handleNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setRecipeName(e.target.value);
+    if (recipes.find((recipe) => recipe.name === e.target.value))
+      setAcceptationDisabled(true);
+    else setAcceptationDisabled(false);
+  };
+
   return (
-    <ListItems<{
-      name: string;
-      description: string | undefined;
-      quantity: number | undefined;
-      unit: string | undefined;
-      id: number;
-    }>
-      url="recipes"
-      renderItem={(item, index) => children}
-      keyExtractor={(item) => item.id.toString()}
-      redirectBasePath="/recipes"
-      headerText="Блюда"
-    />
+    <>
+      <ListItems<{
+        name: string;
+        description: string | undefined;
+        quantity: number | undefined;
+        unit: string | undefined;
+        id: number;
+      }>
+        url="recipes"
+        renderItem={(item, index) => children}
+        keyExtractor={(item) => item.id.toString()}
+        redirectBasePath="/recipes"
+        headerText="Блюда"
+      />
+      <PlusButton
+        onClick={() => {
+          setShowDialog(true);
+        }}
+      />
+      {showDialog && (
+        <Dialog
+          on_add={handleAdd}
+          on_cancel={() => setShowDialog(false)}
+          disabled={acceptationDisabled}>
+          <div className="h-full w-full flex flex-col items-center justify-center">
+            <h1 className="text-2xl font-bold text-white border-b-2 border-yellow-300 mb-8">
+              Добавить блюдо
+            </h1>
+            <input
+              type="text"
+              className="w-7/8 p-2 rounded-md bg-white"
+              placeholder="Название блюда"
+              value={recipeName}
+              onChange={handleNameChange}
+            />
+          </div>
+        </Dialog>
+      )}
+    </>
   );
 }
